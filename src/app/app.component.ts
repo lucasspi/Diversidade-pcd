6b0f68a3eb14cfa7f26e118619bd70c0ecb04906
Lucas Spirandeli <lucasspi@outlook.com>
2019-10-28 22:52:14 -0300

    Ajustes dos modais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import {MatRadioModule} from '@angular/material/radio';
+import { CommonModule } from '@angular/common';
+import {ErrorStateMatcher} from '@angular/material/core';
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,19 +19,36 @@ import { HttpClientModule } from '@angular/common/http';
 export class AppComponent {
   title = 'Diversidade-pcd';
   radioDecision1: any; // rspData = form
-  favoriteSeason: string;
-  seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
+  groupForm: FormGroup;
+  emailForm: FormGroup;
+  public noAuth: true;
+  public favoriteSeason: string;
+  public seasons: string[] = ['RECEBER UMA LIGAÇÃO DA NOSSA CENTRAL', 'PREENCHER O QUESTIONÁRIO PCD'];
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  matcher = new MyErrorStateMatcher();
 
   constructor(cdr: ChangeDetectorRef) {
     // super( http);
+    this.groupForm = this.createForm();
+    this.emailForm = this.createEmailForm();
+  }
+  createForm() {
+    return new FormGroup({
+      radioBox: new FormControl('')
+    });
+  }
+  createEmailForm() {
+    return new FormGroup({
+      name: new FormControl('',[Validators.required]),
+      email: new FormControl('', [Validators.required])
+    });
   }
 
-  onPublishModeChanges($event) {
-    console.log
-    if($event) {
-      this.radioDecision1.publishDate = true
-    } else {
-      this.radioDecision1.publishDate = false;
-    }
+  sendEmail(){
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatRadioModule} from '@angular/material/radio';
+import { CommonModule } from '@angular/common';  
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,11 @@ import {MatRadioModule} from '@angular/material/radio';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatRadioModule
+    MatRadioModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
